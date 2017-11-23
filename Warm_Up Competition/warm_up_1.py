4f14be766c19218141912f0f2c3d6b5888c4fade
Akeraphis <jeanbaptiste.clouard@gmail.com>
2017-11-23 16:53:39 +0100

    Created some plots

--- Warm_Up Competition/warm_up_1.py
+++ Warm_Up Competition/warm_up_1.py
@@ -17,3 +17,27 @@ testing_dt = genfromtxt('testing-data.csv', delimiter=',', skip_header=1)
 np.random.shuffle(full_training_dt)
 training, cv, test = np.split(full_training_dt, [346, 462]);
 
+#Plotting the data
+f, a = plt.subplots(1, 3, sharey=True, tight_layout=True)
+
+#Number of months since last donation
+plt.subplot(3,1,1)
+plt.hist(training[:,1]);
+plt.xlabel('Months since last Donation');
+plt.ylabel('Number of participants');
+plt.title('Months since last donation');
+
+#Number of Donations
+plt.subplot(3,1,2)
+plt.hist(training[:,2]);
+plt.xlabel('Number of Donations');
+plt.ylabel('Number of participants');
+plt.title('Number of Donations');
+
+#Number of Months x Number of Donations scatter
+plt.subplot(3,1,3)
+plt.plot(training[:,1], training[:,2], 'ro');
+plt.xlabel('Monts since last donations');
+plt.ylabel('Number of Donations');
+plt.title('Number of Months since last donation by Number of Donations');
+
