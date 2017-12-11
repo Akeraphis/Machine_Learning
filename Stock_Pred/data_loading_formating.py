c92bfc5ff463b864cd79105b6e5f994375f83245
CLOUARD Jean-Baptiste <JCD1@dsone.3ds.com>
2017-12-11 17:23:27 +0100

    update

--- Stock_Pred/data_loading_formating.py
+++ Stock_Pred/data_loading_formating.py
@@ -5,10 +5,25 @@ Created on Mon Dec 11 09:25:50 2017
 @author: JCD1
 """
 #Load libraries
-import numpy as np
-from numpy import genfromtxt
-from numpy import savetxt
+import os
+import pandas as pd
 
 #Load data files
-A_quarterly_financial_data = genfromtxt(r'C:\Users\jcd1\Documents\GitHub\Machine_Learning\Stock_Pred\data\A_quarterly_financial_data.csv', delimiter=',', skip_header=1)
-AA_quarterly_financial_data = genfromtxt(r'C:\Users\jcd1\Documents\GitHub\Machine_Learning\Stock_Pred\data\AA_quarterly_financial_data.csv', delimiter=',', skip_header=1)
+path = "./Users/jcd1/Documents/GitHub/Machine_Learning/Stock_Pred/data"
+directory = os.path.join("c://",path)
+csv_list = []
+stocks = []
+full_stocks=[]
+
+for root,dirs,files in os.walk(directory):
+    for name in files:
+#        csv_name = pd.DataFrame({'name' : name}, index = range(1))
+        csv_list.append(os.path.join(root, name))
+#        stock_csv = pd.read_csv(os.path.join(root, name))
+#        leng = stock_csv.shape[0]
+#        csv_name.append([csv_name]*leng, ignore_index=True)
+#        stocks = stock_csv.join(csv_name)
+#        full_stocks.concat(stocks)
+       
+#Combined all files into one master file
+combined_csv = pd.concat([pd.read_csv(f) for f in csv_list], axis=0)
